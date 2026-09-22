9809cfa57ab286fc3a668c5fde0a4ce170d5dc5b
Dnsarfa819 <danisharfa579@gmail.com>
2026-09-22 15:06:05 +0700

    feat: tambah semantic html dan navigasi dua halaman

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ promoButton.addEventListener("click", () => {
     promoButton.textContent = "promo : Beli 2 gratis tester!";
     console.log("promo kopi berhasil ditamilkan.")
 });
+
+const headings = [...document.querySelectorAll("h1, h2, h3")];
+
+console.log("Daftar heading pada halaman:");
+headings.forEach((heading) => {
+  console.log(heading.tagName, "-", heading.textContent);
+});
